3fc44ebf648a019fe95335d9ec3ebb07e2235aee
廖捷昕 <liaojiexin@vcmy.com>
2021-04-09 18:00:13 +0800

    运行常量池溢出

--- src/main/java/test/DirectMemoryOOM.java
+++ src/main/java/test/DirectMemoryOOM.java
@@ -1,5 +1,9 @@
 package test;
 
+import sun.misc.Unsafe;
+
+import java.lang.reflect.Field;
+
 /**
  * @ClassName: DirectMemoryOOM
  * @Description: TODO
@@ -10,4 +14,15 @@ package test;
  * @date: 2021/4/9 16:58
  */
 public class DirectMemoryOOM {
+    /*** VM Args：-Xmx20M -XX:MaxDirectMemorySize=10M */
+    private static final int _1MB = 1024 * 1024;
+
+    public static void main(String[] args) throws Exception {
+        Field unsafeField = Unsafe.class.getDeclaredFields()[0];
+        unsafeField.setAccessible(true);
+        Unsafe unsafe = (Unsafe) unsafeField.get(null);
+        while (true) {
+            unsafe.allocateMemory(_1MB);
+        }
+    }
 }
