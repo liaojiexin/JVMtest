c2a58f83e16e0a0b11a016cbc814ffce722a318a
廖捷昕 <liaojiexin@vcmy.com>
2021-04-09 10:35:11 +0800

    java堆溢出

---
+++ src/main/java/test1/JavaVMStackSOF.java
@@ -0,0 +1,33 @@
+package test1;
+
+/**
+ * @ClassName: JavaVMStackSOF
+ * @Description: TODO   虚拟机栈和本地方法溢出溢出
+ * @version: 1.0
+ * @author: liaojiexin
+ * @date: 2021/4/9 10:31
+ */
+public class JavaVMStackSOF {
+
+    /*** VM Args：-Xss128k
+     * 使用-Xss参数减少栈内存容量。
+     *结果：抛出StackOverflowError异常，异常出现时输出的堆栈深度相应缩小。
+     * 定义了大量的本地变量，增大此方法帧中本地变量表的长度。
+     * 结果：抛出StackOverflowError异常，异常出现时输出的堆栈深度相应缩小。 */
+    private int stackLength = 1;
+
+    public void stackLeak() {
+        stackLength++;
+        stackLeak();
+    }
+
+    public static void main(String[] args) throws Throwable {
+        JavaVMStackSOF oom = new JavaVMStackSOF();
+        try {
+            oom.stackLeak();
+        } catch (Throwable e) {
+            System.out.println("stack length:" + oom.stackLength);
+            throw e;
+        }
+    }
+}
