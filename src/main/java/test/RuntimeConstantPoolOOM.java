64cfca958ec5f9313ed045ab14dcd9da5e2a33e3
廖捷昕 <liaojiexin@vcmy.com>
2021-04-09 15:22:36 +0800

    运行常量池溢出

--- src/main/java/test/RuntimeConstantPoolOOM.java
+++ src/main/java/test/RuntimeConstantPoolOOM.java
@@ -12,8 +12,12 @@ import java.util.Set;
  */
 public class RuntimeConstantPoolOOM {
     /*** 在JDK 6或更早之前的HotSpot虚拟机中，常量池都是分配 在永久代中，我们可以通过
-     *  VM Args：-XX:PermSize=6M -XX:MaxPermSize=6M
-     *  限制永久代的大小*/
+     *  VM Args：-XX:PermSize=6M -XX:MaxPermSize=6M限制永久代的大小。
+     *  -----------------------------------------------
+     *  在JDK 7中继续使 用-XX：MaxPermSize参数或者在JDK 8及以上版本使用
+     *  -XX：MaxMeta-spaceSize参数把方法区容量同 样限制在6MB，也都不会重现JDK 6中的溢出异常，
+     *  循环将一直进行下去，永不停歇。jdk7/8以上就要用-Xmx参数限制最大堆到6MB就可以看到溢出异常，
+     *  因为原本存放在永久代的字符串常量池被移至Java堆之中*/
     public static void main(String[] args) {
         // 使用Set保持着常量池引用，避免Full GC回收常量池行为
         Set<String> set = new HashSet<String>();
