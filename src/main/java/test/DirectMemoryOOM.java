610e33a3f6647f031ed6235c5e1706ba8ccf4c7b
廖捷昕 <liaojiexin@vcmy.com>
2021-04-09 17:38:57 +0800

    运行常量池溢出

---
+++ src/main/java/test/DirectMemoryOOM.java
@@ -0,0 +1,13 @@
+package test;
+
+/**
+ * @ClassName: DirectMemoryOOM
+ * @Description: TODO
+ * 本机直接内存溢出  直接内存（Direct Memory）的容量大小可通过
+ * -XX：MaxDirectMemorySize参数来指定，如果不 去指定，则默认与Java堆最大值（由-Xmx指定）一致
+ * @version: 1.0
+ * @author: liaojiexin
+ * @date: 2021/4/9 16:58
+ */
+public class DirectMemoryOOM {
+}
